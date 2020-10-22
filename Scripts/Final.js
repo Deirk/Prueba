7a9fbce262e05c579a646e2128b23b13203ce122
deirk <deirkh@gmail.com>
2020-10-22 14:56:35 -0500

    Final Final Este si, ahora si, ahora si, finalmente si, será que ahora si, ahora si que si, odio programar, aunque creo que ya lo solucione

--- Scripts/Final.js
+++ Scripts/Final.js
@@ -1,25 +1,42 @@
-window.onload = function(){
+window.onload = function () {
 
-    var database = new SaveFirebase("pruebas");
+  var database = new SaveFirebase("pruebas");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-    var mas = document.getElementById("mas");
-    var menos = document.getElementById("menos");
-    var ExperienciaEjecicio1 = document.getElementById("ExperienciaEjecicio1");
-    var ExperienciaEjecicio2 = document.getElementById("ExperienciaEjecicio2");
-    var ExperienciaEjecicio3 = document.getElementById("ExperienciaEjecicio3");
-    var ExperienciaEjecicio4 = document.getElementById("ExperienciaEjecicio4");
-       
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-      sessionStorage.setItem("ExperienciaEjecicio1", ExperienciaEjecicio1.options[ExperienciaEjecicio1.selectedIndex].value);
-      sessionStorage.setItem("ExperienciaEjecicio2", ExperienciaEjecicio2.options[ExperienciaEjecicio2.selectedIndex].value);
-      sessionStorage.setItem("ExperienciaEjecicio3", ExperienciaEjecicio3.options[ExperienciaEjecicio3.selectedIndex].value);
-      sessionStorage.setItem("ExperienciaEjecicio4", ExperienciaEjecicio4.options[ExperienciaEjecicio4.selectedIndex].value);
-      sessionStorage.setItem("LoQueMasLeGusto",mas.value);
-      sessionStorage.setItem("LoQueMenosLeGusto",menos.value);
-      database.save();
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  var mas = document.getElementById("mas");
+  var menos = document.getElementById("menos");
+  var ExperienciaEjecicio1 = document.getElementById("ExperienciaEjecicio1");
+  var ExperienciaEjecicio2 = document.getElementById("ExperienciaEjecicio2");
+  var ExperienciaEjecicio3 = document.getElementById("ExperienciaEjecicio3");
+  var ExperienciaEjecicio4 = document.getElementById("ExperienciaEjecicio4");
+
+  //final.html
+  // When the user clicks the button, open the modal 
+  btn.onclick = function () {
+    sessionStorage.setItem("ExperienciaEjecicio1", ExperienciaEjecicio1.options[ExperienciaEjecicio1.selectedIndex].value);
+    sessionStorage.setItem("ExperienciaEjecicio2", ExperienciaEjecicio2.options[ExperienciaEjecicio2.selectedIndex].value);
+    sessionStorage.setItem("ExperienciaEjecicio3", ExperienciaEjecicio3.options[ExperienciaEjecicio3.selectedIndex].value);
+    sessionStorage.setItem("ExperienciaEjecicio4", ExperienciaEjecicio4.options[ExperienciaEjecicio4.selectedIndex].value);
+    sessionStorage.setItem("LoQueMasLeGusto", mas.value);
+    sessionStorage.setItem("LoQueMenosLeGusto", menos.value);
+    database.save();
+  }
+
+  function isComplete() {
+
+    if (ExperienciaEjecicio1.selectedIndex != 0 && ExperienciaEjecicio2.selectedIndex != 0 && ExperienciaEjecicio3.selectedIndex != 0 &&
+      ExperienciaEjecicio4.selectedIndex != 0 && mas.value != "" && menos.value != "") {
+      console.log("entro");
+      btn.style.background = "#18C3C8"
+      var link = document.getElementById("form__link");
+      link.setAttribute('href', "final.html");
+    } else {
+      btn.style.background = "gray"
     }
-    
-    };
+  }
+
+  setInterval(isComplete, 20);
+
+
+};
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,41 +1,55 @@
-window.onload = function(){
-// Get the modal
-var modal = document.getElementById("myModal");
+window.onload = function () {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  var complete = false;
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var nombre = document.getElementById("nombre");
-var universidad = document.getElementById("universidad");
-var semestre = document.getElementById("semestre");
-var tiempoProgramacion = document.getElementById("tiempo-programacion");
-var carrera = document.getElementById("carrera");
+  // Get the button that opens the modal
+  var btn = document.getElementById("form__btn");
+  var nombre = document.getElementById("nombre");
+  var universidad = document.getElementById("universidad");
+  var semestre = document.getElementById("semestre");
+  var tiempoProgramacion = document.getElementById("tiempo-programacion");
+  var carrera = document.getElementById("carrera");
 
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-
- /*if (nombre.value != null && universidad.value != null && tiempoProgramacion.options[tiempoProgramacion.selectedIndex].value != null && ) {
+  // When the user clicks the button, open the modal 
+  btn.onclick = function () {
+    if (complete == true) {
+      modal.style.display = "block";
+    }
     
-  }*/
-  //Ejercicios.html
-  sessionStorage.setItem("nombre",nombre.value);
-  sessionStorage.setItem("universidad",universidad.value);
-  sessionStorage.setItem("semestre",semestre.value);
-  sessionStorage.setItem("tiempoProgramando", tiempoProgramacion.options[tiempoProgramacion.selectedIndex].value);
-  sessionStorage.setItem("carrera", carrera.value);
-  modal.style.display = "block";
-}
+    //Ejercicios.html
+    sessionStorage.setItem("nombre", nombre.value);
+    sessionStorage.setItem("universidad", universidad.value);
+    sessionStorage.setItem("semestre", semestre.value);
+    sessionStorage.setItem("tiempoProgramando", tiempoProgramacion.options[tiempoProgramacion.selectedIndex].value);
+    sessionStorage.setItem("carrera", carrera.value);
+  }
+  
+    function isComplete() {
+      
+      if (nombre.value != "" && universidad.value != "" && 
+      tiempoProgramacion.selectedIndex != 0 && semestre.value != "" && carrera.value != "") {
+        console.log("entro");
+        complete = true;
+        btn.style.background = "#18C3C8"
+      } else {
+        btn.style.background = "gray"
+      }
+    }
+  
+    setInterval(isComplete,20);
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
     modal.style.display = "none";
   }
-}
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 };
 
